bec2935b59210a52169dc1c2b6a42e5b55538453
NFPerera <niko22602@gmail.com>
2022-05-15 19:21:19 -0300

    Contenido terminado
    Falta corregir el sistema de IDs (Julian y nicolas)
    Y continuar con testeo (Nicolas)

--- Assets/_Main/Scripts/Item/ObjectFood.cs
+++ Assets/_Main/Scripts/Item/ObjectFood.cs
@@ -5,23 +5,29 @@ public class ObjectFood : MonoBehaviour
 {
     [SerializeField] public int ID;
 
-    private LayerMask _playerLayerMask;
+    private LayerMask _plateLayerMask;
 
     private void Start()
     {
-        _playerLayerMask = LayerMask.GetMask("Object");
+        _plateLayerMask = LayerMask.GetMask("Plate");
     }
 
     public void ShootRaycast()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out var hit, 20f,
-                _playerLayerMask))
+        if (Physics.Raycast(transform.position, Vector3.down, out var hit, 20f, _plateLayerMask))
         {
             var plateController = hit.transform.gameObject.GetComponent<PlateController>();
+            Debug.Log(plateController);
             if (plateController != null)
             {
                 plateController.AddFood(gameObject);
             }
         }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, Vector3.down, Color.red);
     }
 }
